6e6fdaa5b3487f332d2a46a12c9c76cd1f0bbc0c
Shajadul <shajadulrahe@gmail.com>
2022-12-19 10:10:48 +0600

    Feat: Programatic navigation form to home

--- src/pages/NewQoutes.js
+++ src/pages/NewQoutes.js
@@ -1,7 +1,10 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 const NewQoutes = () => {
+  const history = useHistory();
   const newQouteHandler = (qoute) => {
     console.log(qoute);
+    history.push("/qoutes");
   };
   return <QuoteForm onAddQuote={newQouteHandler} />;
 };
