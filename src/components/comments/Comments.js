79d57c04e38c7e89ad45aa6006493870151da7bd
Shajadul <shajadulrahe@gmail.com>
2023-01-01 11:04:22 +0600

    Feat: Send comment data to database

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
-
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
-
+  const params = useParams();
+  // console.log(params.qouteID);
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
+      {isAddingComment && <NewCommentForm quoteID={params.qouteID} />}
       <p>Comments...</p>
     </section>
   );
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,13 +1,16 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-import classes from './NewCommentForm.module.css';
-
+import classes from "./NewCommentForm.module.css";
+import { addComment } from "../../lib/api";
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
-
+    addComment({
+      commentData: commentTextRef.current.value,
+      quoteId: props.quoteID,
+    });
     // optional: Could validate here
 
     // send comment to server
@@ -16,11 +19,11 @@ const NewCommentForm = (props) => {
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
