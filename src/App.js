005e7265a37fd038d2807112abedb32c2853597d
Shajadul <shajadulrahe@gmail.com>
2023-01-02 14:29:15 +0600

    Feat: Add Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import AllQoutes from "./pages/AllQoutes";
-import NewQoutes from "./pages/NewQoutes";
-import QouteDetails from "./pages/QouteDetails";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+const NewQoutes = React.lazy(() => import("./pages/NewQoutes"));
+const QouteDetails = React.lazy(() => import("./pages/QouteDetails"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/qoutes" />
-        </Route>
-        <Route path="/qoutes" exact>
-          <AllQoutes />
-        </Route>
-        <Route path="/qoutes/:qouteID">
-          <QouteDetails />
-        </Route>
-        <Route path="/new-qoutes">
-          <NewQoutes />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/qoutes" />
+          </Route>
+          <Route path="/qoutes" exact>
+            <AllQoutes />
+          </Route>
+          <Route path="/qoutes/:qouteID">
+            <QouteDetails />
+          </Route>
+          <Route path="/new-qoutes">
+            <NewQoutes />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
